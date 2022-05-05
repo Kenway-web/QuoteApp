f40e8cb609e1cbcc4acfb7d776b3b32368d70989
Shoaib Ali <shoaibali26021999@gmail.com>
2022-05-05 23:42:56 +0530

    Quiz app created

---
+++ app/src/main/java/eu/kenway/quotesapp/MainViewModel.kt
@@ -0,0 +1,34 @@
+package eu.kenway.quotesapp
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import java.nio.charset.Charset
+
+class MainViewModel(val context: Context):ViewModel() {
+    private var quoteList: Array<Quote> = emptyArray()
+    private var index=0
+
+
+    init {
+        quoteList=loadQuoteFromAsset()
+    }
+
+    private fun loadQuoteFromAsset(): Array<Quote> {
+          val inputStream=context.assets.open("quotes.json")
+          val size:Int=inputStream.available()
+          val buffer=ByteArray(size)
+          inputStream.read(buffer)
+          inputStream.close()
+          val json= String(buffer, Charsets.UTF_8)
+          val gson=Gson()
+          return gson.fromJson(json,Array<Quote>::class.java )
+    }
+
+    fun getQuote()=quoteList[index]
+
+    fun nextQuote()=quoteList[++index]
+
+    fun previousQuote()=quoteList[--index]
+
+}
---
+++ app/src/main/java/eu/kenway/quotesapp/Quote.kt
@@ -0,0 +1,3 @@
+package eu.kenway.quotesapp
+
+data class Quote(val text:String,val author:String)
